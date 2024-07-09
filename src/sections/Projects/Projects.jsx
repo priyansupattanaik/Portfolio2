6d09149feffc9868294470e0fa7dc5bffc0fb287
Priyansu Pattanaik <priyansupattanaikwork@gmail.com>
2024-07-10 01:40:48 +0530

    1

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,24 @@
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png";
+import hipsster from "../../assets/hipsster.png";
+import fitLift from "../../assets/fitlift.png";
+import ProjectCard from "../../common/ProjectCard";
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://bit.ly/thepandorabox/"
+          h3="Arcade"
+          p="Game"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
