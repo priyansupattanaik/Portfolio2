c7a918e2a7d612a946fca76e006f2ddd92ccf999
Priyansu Pattanaik <priyansupattanaikwork@gmail.com>
2024-07-10 13:23:32 +0530

    3

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,10 @@
-import styles from './ContactStyles.module.css';
+import styles from "./ContactStyles.module.css";
 
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form action="https://formspree.io/f/mqazazlz">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
