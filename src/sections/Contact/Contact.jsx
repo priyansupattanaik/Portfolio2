677ad0378675aeac6f4fdce34d9ae0454fdcffa5
Priyansu Pattanaik <priyansupattanaikwork@gmail.com>
2024-07-10 16:08:29 +0530

    4

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="https://formspree.io/f/mqazazlz">
+      <form action="https://formspree.io/f/mqazazlz" method="POST">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
